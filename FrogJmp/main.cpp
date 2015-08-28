fbf8f44562cb973adbfdfd23c28bcfcab032e2d0
Paavo Bennett <eliw00d@medpackstudios.com>
2015-08-28 11:18:46 -0500

    Update main.cpp

--- FrogJmp/main.cpp
+++ FrogJmp/main.cpp
@@ -1,5 +1,6 @@
 #include <cmath>
 
+// 100% Correctness + 100% Performance = 100% Task Score
 int solution(int X, int Y, int D) 
 {        
     return ceil(static_cast<double>(Y - X) / static_cast<double>(D));
